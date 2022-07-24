50df010bfdaed6f23bb46511e2f6dbcbe2a1d41e
Asim Maredia <asimmaredia@Asims-MBP.lan>
2022-07-24 11:52:01 -0500

    adding domain to CSRF origin

--- HappyCampers/settings.py
+++ HappyCampers/settings.py
@@ -122,4 +122,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = ["https://happycampers310.herokuapp.com"]
+
 django_on_heroku.settings(locals())

d0e166808ad45c51df142bbe49642316f4f4dd73
nicolas.cruz <nicocruz@DESKTOP-720M8QF.localdomain>
2022-07-28 11:38:33 -0500

    deleted incorrect models.py file

--- app/models.py
+++ app/models.py
@@ -1,6 +1,121 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey(
+        'DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Camper(models.Model):
     camper_id = models.IntegerField(primary_key=True)
     camper_password = models.CharField(max_length=-1, blank=True, null=True)
@@ -8,8 +123,21 @@ class Camper(models.Model):
     camper_registrationdate = models.DateField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'camper'
+
+
+class Event(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    park = models.ForeignKey(
+        'Park', on_delete=models.CASCADE, blank=True, null=True)
+    event_name = models.CharField(max_length=-1, blank=True, null=True)
+    event_date = models.DateField(blank=True, null=True)
+    event_description = models.CharField(max_length=-1, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'event'
 
 
 class Park(models.Model):
@@ -23,7 +151,7 @@ class Park(models.Model):
     park_sevendaycost = models.FloatField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'park'
 
 
@@ -31,13 +159,14 @@ class Reservation(models.Model):
     reservation_id = models.IntegerField(primary_key=True)
     camper = models.ForeignKey(
         Camper, models.DO_NOTHING, blank=True, null=True)
-    park = models.ForeignKey(Park, models.DO_NOTHING, blank=True, null=True)
+    park = models.ForeignKey(
+        Park, on_delete=models.CASCADE, blank=True, null=True)
     reservation_startdate = models.DateField(blank=True, null=True)
     reservation_enddate = models.DateField(blank=True, null=True)
     reservation_totalcost = models.FloatField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'reservation'
 
 
@@ -45,10 +174,11 @@ class Review(models.Model):
     review_id = models.IntegerField(primary_key=True)
     camper = models.ForeignKey(
         Camper, models.DO_NOTHING, blank=True, null=True)
-    park = models.ForeignKey(Park, models.DO_NOTHING, blank=True, null=True)
+    park = models.ForeignKey(
+        Park, on_delete=models.CASCADE, blank=True, null=True)
     review_date = models.DateField(blank=True, null=True)
     review_rating = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'review'

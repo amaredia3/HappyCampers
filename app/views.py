1c9f4021eb8df043aed3571757fc04386d613b35
Asim Maredia <70919443+amaredia3@users.noreply.github.com>
2022-07-31 18:49:11 -0500

    reservations page can create reservations (#9)

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from multiprocessing import Event
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,6 +7,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
 import datetime
 from django.db.models import Max
+from datetime import datetime
 
 def login(request):
     error_message = ''
@@ -20,6 +22,7 @@ def login(request):
             error_message = 'Account for this email does not exist'
         else:
             if check_password(password,user_password_encrypted):
+                request.session['user-id'] = Camper.objects.filter(camper_email=email).only('camper_id')[0].camper_id
                 return render(request, 'home.html')
             else:
                 error_message = "Invalid Password"
@@ -39,6 +42,8 @@ def nationalParks(request):
     park_list = Park.objects.filter(park_name='Yosemite National Park')
     event_list = Event.objects.filter(park_id='1')
 
+    request.session['park-id'] = park_list[0].park_id
+
     if request.method == 'POST':
         newRating = Review()
         newRating.review_rating = request.POST.get('rating-dropdown')
@@ -56,7 +61,52 @@ def nationalParks(request):
 
 
 def reservations(request):
-    return render(request, 'reservations.html')
+    error_message = ''
+    if 'park' in request.session:
+        national_park = Park.objects.filter(park_id=request.session['park-id'])[0]
+    else:
+        national_park = ''
+    estimated_cost = ''
+    reservations = ''
+    start_date = None
+    end_date = None
+    if request.method == "POST":
+        try:
+            start_date = datetime.strptime(request.POST['start-date'], '%m/%d/%Y')
+            end_date = datetime.strptime(request.POST['end-date'],'%m/%d/%Y')
+        except:
+            error_message = "Invalid date format. Try Again."
+            return render(request, 'reservations.html', { 'national_park': national_park.park_name, 'error_message': error_message, 'estimated_cost': estimated_cost})
+        try:
+            time_delta = end_date - start_date
+            if time_delta.days < 0:
+                raise Exception("Start and End dates are not valid.")
+        except:
+            error_message = 'Incompatible Start and End Dates.'
+            return render(request, 'reservations.html', { 'national_park': national_park.park_name, 'error_message': error_message, 'estimated_cost': estimated_cost})
+        try:
+            last_id = Reservation.objects.aggregate(Max('reservation_id'))
+            if last_id['reservation_id__max'] == None:
+                last_id['reservation_id__max'] = 0
+            time_delta = end_date - start_date
+            cost = national_park.park_sevendaycost * (time_delta.days / 7.0)
+            estimated_cost = cost
+            newReservation = Reservation(
+                                            park=national_park,
+                                            camper=Camper.objects.filter(camper_id=request.session['user-id'])[0],
+                                            reservation_id=last_id['reservation_id__max']+1,
+                                            reservation_startdate=start_date,
+                                            reservation_enddate = end_date,
+                                            reservation_totalcost=cost)
+            newReservation.save()
+        except:
+            error_message = 'Internal Error. Please try again.'
+    try:
+        reservations = Reservation.objects.filter(camper=Camper.objects.filter(camper_id=request.session['user-id'])[0],
+                                                park=national_park)
+    except:
+        error_message = 'Could not get your reservations.'
+    return render(request, 'reservations.html', { 'national_park': national_park.park_name, 'error_message': error_message, 'estimated_cost': estimated_cost, 'reservations': reservations})
 
 
 def signup(request):
@@ -72,6 +122,7 @@ def signup(request):
             try:
                 newUser = Camper(camper_id=last_id['camper_id__max']+1,camper_email=email,camper_password=make_password(password),camper_registrationdate=datetime.date.today(),camper_name=name)
                 newUser.save()
+                request.session['user-id'] = Camper.objects.filter(camper_email=email).only('camper_id')[0].camper_id
                 return render(request,'home.html')
             except:
                 error_message = 'Internal Error. Please try Again.'

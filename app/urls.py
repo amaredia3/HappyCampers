aa8af7d32bb2be495d3c56f4df4d8bf9ccdf0515
Brendan Fattig <54815652+brendanfattig@users.noreply.github.com>
2022-08-08 21:31:09 -0500

    add comments and refine logout functionality (#42)

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.login, name='login'),
-path('home',views.home, name='home'),
-path('parks',views.parks, name='parks'),
-path('reservations',views.reservations, name='reservations'),
-path('updateReservations/<int:reservationID>/',views.updateReservations, name='updateReservations'),
-path('nationalParks/<int:parkID>/',views.nationalParks, name='nationalParks'),
-path('signup',views.signup, name='signup'),
-path('changePassword',views.changePassword, name='changePassword')
+    path('', views.login, name='login'),
+    path('home',views.home, name='home'),
+    path('parks',views.parks, name='parks'),
+    path('reservations',views.reservations, name='reservations'),
+    path('updateReservations/<int:reservationID>/',views.updateReservations, name='updateReservations'),
+    path('nationalParks/<int:parkID>/',views.nationalParks, name='nationalParks'),
+    path('signup',views.signup, name='signup'),
+    path('changePassword',views.changePassword, name='changePassword'),
+    path('logout',views.logout, name='logout')
 ]
